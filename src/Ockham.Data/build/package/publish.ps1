72a931b230305be1355a7168a9cc42cf56c3b4e2
Joshua Honig <joshua.honig@gmail.com>
2017-10-20 15:46:12 -0400

    Added Ockham.Data

---
+++ src/Ockham.Data/build/package/publish.ps1
@@ -0,0 +1,17 @@
+﻿$thisDir = (Get-Item (Get-Location)).FullName
+Write-Host "Current dir: $thisDir"
+
+$xNuspec = New-Object xml
+$xNuspec.Load('Ockham.Data.nuspec')
+$xPackage = $xNuspec.DocumentElement 
+$version = $xPackage.metadata.version.Trim()
+$id = $xPackage.metadata.id
+ 
+[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+$apiKey = ([string]([Microsoft.VisualBasic.Interaction]::InputBox('Enter the ockham.net nuget.org API key', 'Enter API Key'))).Trim()
+
+nuget pack
+
+$nupkgPath = "$id.$version.nupkg"
+
+nuget push $nupkgPath -ApiKey $apiKey -s nuget.org
